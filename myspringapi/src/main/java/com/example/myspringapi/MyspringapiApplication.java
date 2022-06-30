26d377d67b7a790cfd1353c34e5334bb8dd53233
Alexandru Neacsa <Alexandru-Valentin.Neacsa@Endava.com>
2022-06-30 06:09:43 +0300

    V1 with jar

--- myspringapi/src/main/java/com/example/myspringapi/MyspringapiApplication.java
+++ myspringapi/src/main/java/com/example/myspringapi/MyspringapiApplication.java
@@ -5,8 +5,6 @@ package com.example.myspringapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MyspringapiApplication {
